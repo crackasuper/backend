92decc57a6abc421343efc56be08304d0f606b64
crackasuper <crackabad859@gmail.com>
2024-07-19 14:20:43 +0300

    preparing models to create databases

--- students/models.py
+++ students/models.py
@@ -1,3 +1,13 @@
 from django.db import models # type: ignore
 
 # Create your models here.
+
+class Student(models.Model):
+    studentID = models.AutoField(primary_key=True)
+    FirstName = models.CharField(max_length=50)
+    LastName = models.CharField(max_length=50)
+    Email = models.EmailField(max_length=50)
+    RegistrationNo = models.CharField(max_length=50)
+    Course = models.CharField(max_length=50)
+
+    Semester = models.CharField(max_length=50)
