388720a7da0c4a532f2b55967643db4b073d9d05
crackasuper <crackabad859@gmail.com>
2024-07-27 07:06:13 +0300

    preparing home background photo and navigations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from "./components/Navigation";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./components/Home";
+
 
 function App() {
   return (
-    <Navigation />
+    <BrowserRouter>
+      <Navigation/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+      </Routes>
+
+    </BrowserRouter>
+    
     );
 }
 export default App;
